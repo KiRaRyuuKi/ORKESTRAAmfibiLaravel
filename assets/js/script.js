2f00ca1ec526027e441b55c87cdea99febe8487b
Muhammad Ilham <M.Ilham.v.28.07.2003@gmail.com>
2023-11-02 22:00:32 +0700

    Penyesuaian scrollreveal dan menambahkan showroom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
-      navToggle = document.getElementById("nav-toggle"),
-      navClose = document.getElementById("nav-close")
+     navToggle = document.getElementById("nav-toggle"),
+     navClose = document.getElementById("nav-close")
 
 if (navToggle) {
      navToggle.addEventListener("click", () => {
@@ -19,7 +19,7 @@ if (navClose) {
 const navLink = document.querySelectorAll(".nav-link")
 
 const linkAction = () => {
-     const navMenu =document.getElementById("nav-menu")
+     const navMenu = document.getElementById("nav-menu")
      navMenu.classList.remove("show-menu")
 }
 navLink.forEach(n => n.addEventListener("click", linkAction))
@@ -28,15 +28,15 @@ navLink.forEach(n => n.addEventListener("click", linkAction))
 const scrollHeader = () => {
      const header = document.getElementById("header")
      this.scrollY >= 50 ? header.classList.add("scroll-header")
-                        : header.classList.remove("scroll-header")
+          : header.classList.remove("scroll-header")
 }
 window.addEventListener("scroll", scrollHeader)
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
      const scrollUp = document.getElementById("scroll-up")
      this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
-                         : scrollUp.classList.remove("show-scroll")
+          : scrollUp.classList.remove("show-scroll")
 }
 window.addEventListener("scroll", scrollUp)
 
@@ -48,10 +48,10 @@ const scrollActive = () => {
 
      sections.forEach(current => {
           const sectionHeight = current.offsetHeight,
-                sectionTop = current.offsetTop - 58,
-                sectionId = current.getAttribute("id"),
-                sectionClass = document.querySelector(".nav-menu a[href*=" + sectionId + "]")
-          
+               sectionTop = current.offsetTop - 58,
+               sectionId = current.getAttribute("id"),
+               sectionClass = document.querySelector(".nav-menu a[href*=" + sectionId + "]")
+
           if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
                sectionClass.classList.add("active-links")
           } else {
@@ -62,34 +62,34 @@ const scrollActive = () => {
 
 window.addEventListener("scroll", scrollActive)
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById("theme-button")
 const darkTheme = "dark-theme"
 const iconTheme = "ri-sun-line"
 
-const selectedTheme = localStorage.getItem("selected-theme") 
+const selectedTheme = localStorage.getItem("selected-theme")
 const selectedIcon = localStorage.getItem("selected-icon")
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light" 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line"
 
 if (selectedTheme) {
-     document.body.classList[selectedTheme === "dark" ? "add" : "remove"] (darkTheme)
+     document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
      themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
 }
 
 themeButton.addEventListener("click", () => {
-     document.body.classList.toggle(darkTheme) 
+     document.body.classList.toggle(darkTheme)
      themeButton.classList.toggle(iconTheme)
 
-     localStorage.setItem("selected-theme", getCurrentTheme()) 
+     localStorage.setItem("selected-theme", getCurrentTheme())
      localStorage.setItem("selected-icon", getCurrentIcon())
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const scr = ScrollReveal({
-     origin: "top", 
-     distance: "60px", 
+     origin: "top",
+     distance: "60px",
      duration: 1500,
      delay: 250,
 })
