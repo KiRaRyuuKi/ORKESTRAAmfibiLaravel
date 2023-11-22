8945e1b6dcef9b9baf83f2c3339a5019c9a95730
A4nasui <anassaifulloh765>
2023-11-22 14:26:34 +0700

    Administrasi Paycars

---
+++ Administrasi Paycars/Pengguna/delete.php
@@ -0,0 +1,13 @@
+<?php
+include "../koneksi/koneksi.php";
+
+$id = $_GET["id_pengguna"];
+$sql = "DELETE FROM data_pengguna WHERE `data_pengguna`.`id_pengguna` = '$id'";
+$result = mysqli_query($koneksi, $sql);
+
+if ($result) {
+    header("Location: Data_Pengguna.php?msg=Data deleted successfully");
+} else {
+    echo "Failed: " . mysqli_error($koneksi);
+}
+?>
